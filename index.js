00498caab4bec82c5f83349077344ecf76887a12
Dlzheen Mahmood <dlzheenmahmood@gmail.com>
2021-04-20 19:52:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,8 +19,12 @@ function unleashDog(dogName,dogBreed){
 let routine = [wakeDog, leashDog, walkToPark,throwFrisbee,walkHome,unleashDog]; 
 
 function exerciseDog(dogName,dogBreed){
+ let routine.wakeDog(dogName,dogBreed); 
+ routine.leashDog(dogName,dogBreed); 
+ routine.walkToPark(dogName,dogBreed); 
+ routine.throwFrisbee(dogName,dogBreed); 
+ routine.walkHome(dogName,dogBreed); 
  routine.unleashDog(dogName,dogBreed); 
-  
  
 }
 const executedMessages = exerciseDog("Esther", "Dalmation");
